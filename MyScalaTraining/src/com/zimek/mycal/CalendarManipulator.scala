1fbb249f08a0c5cefec201bd0491c8d91c8ad81f
mzimecki <marcin.zimecki@gmail.com>
2013-06-26 08:29:57 +0200

    - comments added, for loop refactored

--- MyScalaTraining/src/com/zimek/mycal/CalendarManipulator.scala
+++ MyScalaTraining/src/com/zimek/mycal/CalendarManipulator.scala
@@ -1,10 +1,10 @@
 package com.zimek.mycal
 
 import java.text.SimpleDateFormat
-import java.util.GregorianCalendar
 import java.util.Calendar
+import java.util.GregorianCalendar
 import java.util.Locale
-import scala.util.control.Breaks
+
 import scala.collection.mutable.LinkedHashMap
 
 /**
@@ -13,17 +13,24 @@ import scala.collection.mutable.LinkedHashMap
 object CalendarManipulator {
 	val cal = new GregorianCalendar()
 	val sdf = new SimpleDateFormat("dd MMMM yyyy", Locale.US)
-	val weekDays = LinkedHashMap[Int, Int]((1 -> Calendar.MONDAY), (2 -> Calendar.TUESDAY), (3 -> Calendar.WEDNESDAY), (4 -> Calendar.THURSDAY),
-								 (5 -> Calendar.FRIDAY), (6 -> Calendar.SATURDAY), (7 -> Calendar.SUNDAY))
+	val weekDays = LinkedHashMap[Int, Int]((1 -> Calendar.MONDAY), 
+			(2 -> Calendar.TUESDAY), (3 -> Calendar.WEDNESDAY), (4 -> Calendar.THURSDAY),
+			(5 -> Calendar.FRIDAY), (6 -> Calendar.SATURDAY), (7 -> Calendar.SUNDAY))
 	
+	/**
+	 * Prints basic data of current date.
+	 */
 	def printBasicData() { 
 		println("Today is: " + sdf.format(cal.getTime()) + ", week: " + cal.getMaximum(Calendar.WEEK_OF_YEAR)) 
 	} 
 	
+	/**
+	 * Prints months day numbers.
+	 */
 	def printMonth() {
 		cal.set(Calendar.DAY_OF_MONTH, 1)
 		weekDays.foreach(printDayAsString)
-		val startingDayIdx = getDayIndex(cal.get(Calendar.DAY_OF_WEEK), weekDays)
+		val startingDayIdx = getDayIndex(cal.get(Calendar.DAY_OF_WEEK), weekDays.keysIterator)
 		for(i <- 1 until startingDayIdx) print("    ")
 		for (i <- 1 to cal.getActualMaximum(Calendar.DAY_OF_MONTH)) {
 			if (i < 10)
@@ -34,21 +41,25 @@ object CalendarManipulator {
 		}
 	}
 	
-	//TODO: should be done different I think
-	private def getDayIndex(dayOfAweek: Int, days: LinkedHashMap[Int, Int]) : Int = {
-		val loop = new Breaks;
-		var k = -1;
-		loop.breakable {
-			for(key <- days.keySet) {
-				if(days.getOrElse(key, -1) == dayOfAweek) {
-					k = key
-					loop.break
-				}
-			}
-		}
-		k
+	/**
+	 * Returns index (map key) of given day.
+	 * 
+	 * @param dayOfAweek Day constant from {@link: Calendar} class
+	 * @param keysIterator Map keys iterator
+	 */
+	private def getDayIndex(dayOfAweek: Int, keysIterator: Iterator[Int]) : Int = {
+		if(keysIterator.hasNext) {
+			val key = keysIterator.next
+			if(weekDays.getOrElse(key, -1) == dayOfAweek) key
+			else getDayIndex(dayOfAweek, keysIterator)
+		} else -1
 	}
 	
+	/**
+	 * Prints day name.
+	 * 
+	 * @param data data Tuple of index and day constant
+	 */
 	private def printDayAsString(data: (Int, Int)) {
 		data match {
 			case (1, Calendar.MONDAY) => print("Mon ")
