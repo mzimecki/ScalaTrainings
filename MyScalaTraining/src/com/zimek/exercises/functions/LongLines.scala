4aeb40e8ee6ef72f19fbdfb3c13edeaf8a9530b4
mzimecki <marcin.zimecki@gmail.com>
2013-06-27 12:35:21 +0200

    - chapter 8 examples added

--- MyScalaTraining/src/com/zimek/exercises/functions/LongLines.scala
+++ MyScalaTraining/src/com/zimek/exercises/functions/LongLines.scala
@@ -8,7 +8,7 @@ import scala.io.Source
 object LongLines {
 	def processFile(filename: String, width: Int) {
 
-		def processLine(line: String) { //local function - it has acces to outer function parameters
+		def processLine(line: String) { //local function - it has access to outer function parameters
 			if (line.length > width)
 				println(filename + ": " + line.trim)
 		}
@@ -21,8 +21,33 @@ object LongLines {
 
 object FindLongLines {
 	def main(args: Array[String]) {
+		
+		var increase = (x: Int) => x + 1 //function literal assigned to var
+		
+		var increase2 = (x: Int) => {
+			println("one")
+			println("two")
+			x + 1
+		}
+						
 		val width = args(0).toInt
 		for (arg <- args.drop(1))
 			LongLines.processFile(arg, width)
+			
+		val l = List[Int](1, 2, 3, 4, 5)
+		l.foreach((x: Int) => print(" " + (x + 1))) //function literal used as argument of other function
+		print("\n")
+		l.filter((x: Int) => x > 3).foreach(println)
+		l.filter(x => x > 3).foreach(println) //shorter form of function literal (target typing) - type of x is inferred
+		l.filter(_ > 2).foreach(println) // placeholder syntax
+		l.filter((_: Int) > 2).foreach(println) // placeholder syntax
+		print("\n")
+		l.foreach(println _) //partially applied function
+		val a = sum _ //partially applied function
+		println(a(1, 2, 4)) //then a refers to a function value object which can be called like this
+		val b = sum(1, _: Int, 4) //other example
+		println(b(2))
 	}
+	
+	def sum(a: Int, b: Int, c: Int) = a + b + c
 } 
