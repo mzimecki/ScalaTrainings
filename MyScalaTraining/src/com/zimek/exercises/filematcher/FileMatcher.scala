dd2adf58000b8c9f23b8f1707b1baae097185d03
mzimecki <marcin.zimecki@gmail.com>
2013-07-02 09:28:16 +0200

    - chapter 9 examples added

--- MyScalaTraining/src/com/zimek/exercises/filematcher/FileMatcher.scala
+++ MyScalaTraining/src/com/zimek/exercises/filematcher/FileMatcher.scala
@@ -45,6 +45,15 @@ object FileMatcher {
 		}
 	}
 
+	def withPrintWriter2(file: File)(op: PrintWriter => Unit) { //arguments as 2 separate lists
+		val writer = new PrintWriter(file)
+		try {
+			op(writer)
+		} finally {
+			writer.close()
+		}
+	}
+
 	def main(args: Array[String]) {
 		//curried functions call
 		println(curriedSum(1)(2))
@@ -52,10 +61,46 @@ object FileMatcher {
 		println(onePlus(3))
 
 		println(twice(_ + 1, 2))
-		
+
+		//new control structures
 		withPrintWriter(
 			new File("date.txt"),
 			writer => writer.println(new java.util.Date))
+
+		println { "Hello, world!" } //{} can be used instead of () to pass function's argument if there is only one argument	
+
+		val file = new File("date.txt")
+
+		//in this call one parameter is in {} so it looks like new control structure
+		withPrintWriter2(file) {
+			writer => writer.println(new java.util.Date)
+		}
+
+		//by-name parameters
+
+		var assertionsEnabled = true
+
+			//without by-name parameters
+			def myAssert(predicate: () => Boolean) =
+				if (assertionsEnabled && !predicate())
+					throw new AssertionError
+		myAssert(() => 5 > 3) //strange call is then
+
+			//by-name version
+			def byNameAssert(predicate: => Boolean) =
+				if (assertionsEnabled && !predicate)
+					throw new AssertionError
+		byNameAssert(5 > 3) //now we can omit () => //5 > 3 will be evaluated inside byNameAssert
+			
+			//with Boolean parameter
+			def boolAssert(predicate: Boolean) =
+				if (assertionsEnabled && !predicate)
+					throw new AssertionError
+		boolAssert(5 > 3) //5 > 3 will be evaluated before boolAssert call 
+		//this means if assertionEnabled = false then
+		//byNameAssert(x / 0 == 0) will not raise an exception while
+		//boolAssert(x / 0 == 0) will raise exception
+		
 
 	}
 }
