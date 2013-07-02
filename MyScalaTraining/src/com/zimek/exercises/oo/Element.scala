2fff7ab92d5598fdf56950877a8a57b1ecb16f53
mzimecki <marcin.zimecki@gmail.com>
2013-07-02 19:48:52 +0200

    - chapter 10 started

---
+++ MyScalaTraining/src/com/zimek/exercises/oo/Element.scala
@@ -0,0 +1,8 @@
+package com.zimek.exercises.oo
+
+/**
+ * Chapter 10 examples
+ */
+abstract class Element {
+  def contents: Array[String] //abstract method - no abstract keyword is needed, returns Array[String]
+}
