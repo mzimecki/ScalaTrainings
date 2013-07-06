abe8a1848a9b18a368d43d2d2d26ea39c4cb6e8a
mzimecki <marcin.zimecki@gmail.com>
2013-07-06 14:50:13 +0200

    - ch. 10 examples added

--- MyScalaTraining/src/com/zimek/exercises/oo/ArrayElement.scala
+++ MyScalaTraining/src/com/zimek/exercises/oo/ArrayElement.scala
@@ -1,6 +1,9 @@
 package com.zimek.exercises.oo
 
 class ArrayElement(val contents: Array[String]) extends Element {
-	// val contents: Array[String] = conts //this is possible in Scala - override method with val with the same name
+  // val contents: Array[String] = conts //this is possible in Scala - override method with val with the same name
   //now val contents as class parameter will override Element's abstract contents method! (override methods by vals)
+  final def demo() {
+    println("ArrayElement's implementation invoked")
+  }
 }
