f9e93f277d10ee8133f534f09a431c192bbcd5b9
mzimecki <marcin.zimecki@gmail.com>
2013-08-28 11:46:27 +0200

    - chapter 16 (lists) examples added

---
+++ MyScalaTraining/src/com/zimek/exercises/lists/Main.scala
@@ -0,0 +1,30 @@
+package com.zimek.exercises.lists
+
+object Main {
+  def main(args: Array[String]) {
+    val fruit: List[String] = List("apples", "oranges", "pears")
+    val nums: List[Int] = List(1, 2, 3, 4)
+    val diag3: List[List[Int]] =
+      List(
+        List(1, 0, 0),
+        List(0, 1, 0),
+        List(0, 0, 1))
+    val empty: List[Nothing] = List()
+
+    val fruit2 = "apples" :: ("oranges" :: ("pears" :: Nil))
+    val nums2 = 1 :: (2 :: (3 :: (4 :: Nil)))
+    val diag32 = (1 :: (0 :: (0 :: Nil))) ::
+      (0 :: (1 :: (0 :: Nil))) ::
+      (0 :: (0 :: (1 :: Nil))) :: Nil
+    val empty2 = Nil
+
+    val z = "a" :: ("b", "c") :: "f" :: Nil
+    for (s <- z) {
+      println(s)
+    }
+    
+    println(z.head)
+    println(z.tail) //returns a list consisting of all elements except the first
+
+  }
+}
